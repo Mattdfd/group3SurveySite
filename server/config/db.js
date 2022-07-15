ceb1bf92319c715aba0442ea1c9fc5f413c5d350
Matthew Mikhaiel <MattDFD@live.ca>
2022-07-16 01:09:41 +0200

    DB connection established

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb+srv://mattdfd:W5QXMcdKnFL7U9mj@assignment2.bv2qf.mongodb.net/?retryWrites=true&w=majority"
+  // Username: Grp3Survey | Password: H0UFcEHhwtHfJBbT
+  "URI": "mongodb+srv://Grp3Survey:H0UFcEHhwtHfJBbT@cluster0.lflkn0d.mongodb.net/test"
 };
